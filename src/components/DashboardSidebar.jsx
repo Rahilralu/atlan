65c475ed388a31bea46d99d5d2db59a96095e243
Rahilralu <nazarrahil0000@gmail.com>
2025-09-14 14:39:10 +0530

    Final last

--- src/components/DashboardSidebar.jsx
+++ src/components/DashboardSidebar.jsx
@@ -9,7 +9,6 @@ const DashboardSidebar = ({ chatOpen, setChatOpen }) => {
       aria-label="Open chat"
       className="fixed bottom-6 right-6 cursor-pointer z-50"
     >
-      {/* White circular wrapper */}
       <div className="w-14 h-14 rounded-full bg-white flex items-center justify-center shadow-lg hover:scale-110 transition-transform">
         <img
           src="/bot1.png"
